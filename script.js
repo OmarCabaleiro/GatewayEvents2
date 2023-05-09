3220ca4b8b89d409c3ca347c73faf91cd46e0c9f
Omar Cabaleiro <51969062+OmarCabaleiro@users.noreply.github.com>
2023-05-09 12:16:54 -0400

    Added several videos to array

--- script.js
+++ script.js
@@ -8,8 +8,21 @@ var videoContainer = document.getElementById('videoContainer'),
     ],
     vidSources =
     [
-        "videos/dance-1678.mp4",
+        "videos/crowd-15818.mp4",
+        "videos/dancing-15706.mp4",
         "videos/nightclub-15802.mp4",
+        "videos/Girl - 15705.mp4",
+        "videos/Girl - 15936.mp4",
+        "videos/Girl - 29313.mp4",
+        "videos/pexels-denys-gromov-5387493-1920x1080-50fps.mp4",
+        "videos/pexels-denys-gromov-7054465-1920x1080-50fps.mp4",
+        "videos/pexels-mart-production-7269159-3840x2160-25fps.mp4",
+        "videos/pexels-mart-production-7271837-3840x2160-25fps.mp4",
+        "videos/pexels-mikhail-nilov-8059634-1920x1080-25fps.mp4",
+        "videos/pexels-stefwithanf-4043974-1920x1080-24fps.mp4",
+        "videos/pexels-stefwithanf-4043983-1920x1080-24fps.mp4",
+        "videos/pexels-stefwithanf-4044009-1920x1080-24fps.mp4",
+        "videos/Woman - 124868.mp4"
     ],
     nextActiveVideo = Math.floor((Math.random() * vidSources.length));
     
